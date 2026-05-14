d8f19899fc8cad761c971f6f11065579f16b372e
leeosilvp <leo965679@gmail.com>
2026-05-14 16:17:50 -0300

    fix: problema com SPA

---
+++ src/routes/Erro.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+
+const Erro = () => {
+    return (
+        <main className="erro-main">
+            <Header />
+            <section className="erro-content">
+                
+            </section>
+            <Footer />
+        </main>
+    )
+}
+
+export default Erro
